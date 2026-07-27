4a33de485e1f9d8f52f6b4c4500914c978641d13
Bekir Yılmaz <bekiryilmaz.ce@gmail.com>
2026-07-27 16:03:22 +0200

    number utility

---
+++ src/NumberUtility.ts
@@ -0,0 +1,14 @@
+export const NumberUtility = {
+  FormatDecimal: (value: number, fractionDigits: number = 2): string =>
+    new Intl.NumberFormat("tr-TR", {
+      minimumFractionDigits: fractionDigits,
+      maximumFractionDigits: fractionDigits,
+    }).format(value),
+
+  FormatInteger: (value: number): string => new Intl.NumberFormat("tr-TR", { maximumFractionDigits: 0 }).format(value),
+
+  ParseDecimal: (value: string): number => {
+    const normalized = value.replace(/\s/g, "").replace(/\./g, "").replace(",", ".");
+    return Number(normalized);
+  },
+};
