d9af1260fa381292cefb63bc329b5cf6918c3dd4
Bekir Yılmaz <bekiryilmaz.ce@gmail.com>
2026-07-27 16:03:33 +0200

    postalcode utility

---
+++ src/PostalCodeUtility.ts
@@ -0,0 +1,5 @@
+export const PostalCodeUtility = {
+  IsValid: (value: string): boolean => /^[0-8]\d{4}$/.test(value),
+
+  Normalize: (value: string): string => (value ? value.replace(/\D/g, "").slice(0, 5) : value),
+};
